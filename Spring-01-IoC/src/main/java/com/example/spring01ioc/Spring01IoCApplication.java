71f2a2a6f8ff11e89093ff67f93dc19a2d2ddf79
12816 <aybolek27@gmail.com>
2022-03-05 21:19:48 -0600

    Initial commit

---
+++ Spring-01-IoC/src/main/java/com/example/spring01ioc/Spring01IoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring01ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01IoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring01IoCApplication.class, args);
+    }
+
+}
---
+++ Spring-01-IoC/src/test/java/com/example/spring01ioc/Spring01IoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring01ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01IoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
